59d3a2af1b1e4a59255e49bcb4101a7ad194e26a
canhnd <canhnd15@gmail.com>
2024-10-27 10:47:44 +0700

    native queries example

--- src/test/java/com/davidnguyen/querytypes/SpringDataJpaQueryTypesApplicationTests.java
+++ src/test/java/com/davidnguyen/querytypes/SpringDataJpaQueryTypesApplicationTests.java
@@ -1,13 +1,27 @@
 package com.davidnguyen.querytypes;
 
+import com.davidnguyen.querytypes.user.User;
+import com.davidnguyen.querytypes.user.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 class SpringDataJpaQueryTypesApplicationTests {
+    @Autowired
+    private UserRepository userRepository;
 
     @Test
-    void contextLoads() {
+    void testNativeQueries() {
+        List<User> usersByStatus = userRepository.findUsersByStatus("ACTIVE");
+
+        assertEquals(2, usersByStatus.size(), "Expected two users with status ACTIVE");
     }
 
 }
